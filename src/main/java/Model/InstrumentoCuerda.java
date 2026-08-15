997c519ab874e08785abdffef5eb9ed853516da1
cossi <cossi@DESKTOP-23HCJF3>
2026-08-15 10:34:23 -0500

    Clases model y servicios

---
+++ src/main/java/Model/InstrumentoCuerda.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author cossi
+ */
+public class InstrumentoCuerda extends Instrumento{
+    
+    private int numeroCuerdas;
+    private int numeroTrastes;
+    
+    public InstrumentoCuerda(int id, String nombre, LocalDate fechaVenta, double precio, int numeroCuerdas, int numeroTrastes)throws Exception
+    {
+        super(id, nombre, fechaVenta, precio);
+        setNumeroCuerdas(numeroCuerdas);
+        this.numeroTrastes = numeroTrastes;
+    }
+    
+    public int getNumeroCuerdas()
+    {
+        return numeroCuerdas;
+    }
+    
+    public void setNumeroCuerdas(int numeroCuerdas) throws Exception
+    {
+        if(numeroCuerdas > 0)
+        {
+            this.numeroCuerdas = numeroCuerdas;
+        }
+        else
+        {
+            throw new Exception("Numero de cuerdas menor o igual a 0");
+        }
+    }
+    
+    public int getNumeroTrastes()
+    {
+        return numeroTrastes;
+    }
+    
+    public void setNumeroTrastes(int numeroTrastes)
+    {
+        this.numeroTrastes = numeroTrastes;
+    }
+}
