b69fa5e887690f6b26c39d6510ef214947df9ed3
cossi <cossi@DESKTOP-23HCJF3>
2026-08-15 09:09:39 -0500

    First commit

---
+++ src/main/java/com/mycompany/ventainstrumentosmusicales/VentaInstrumentosMusicales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ventainstrumentosmusicales;
+
+/**
+ *
+ * @author cossi
+ */
+public class VentaInstrumentosMusicales {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
